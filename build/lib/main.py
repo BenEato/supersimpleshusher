3dfb1c91ea5f11e320b678cb53745a68345407b7
pi <greb88@gmail.com>
2021-04-03 19:07:55 +1100

    Updated

---
+++ build/lib/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import subprocess
+
+app = Flask(__name__)
+
+#hell = print("hello")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/start_music', methods=['POST'])
+def my_form_post():
+    subprocess.call("./startmusic.sh")
+    a = "Turned on"
+    return render_template("index.html",output=a)
+
+@app.route('/stop_music', methods=['POST'])
+def stop_my_music():
+    subprocess.call("./stopmusic.sh")
+    b = "Turned off"
+    return render_template("index.html",output=b)
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
+   #app.run(host='0.0.0.0', port=80)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aviv',
+    version='1',
+    packages=[''],
+    url='beneato.org',
+    license='free',
+    author='pi',
+    author_email='ben@beneato.org',
+    description='supersimpleshusher'
+)
